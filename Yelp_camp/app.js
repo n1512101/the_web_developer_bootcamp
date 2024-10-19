fc272ae4eb405918c2e7a63b162b37bb3ada984d
haku <ngq299@gmail.com>
2024-10-19 12:03:36 +0900

    final commit

--- Yelp_camp/app.js
+++ Yelp_camp/app.js
@@ -16,6 +16,7 @@ const flash = require('connect-flash')
 const passport = require('passport')
 const LocalStrategy = require('passport-local')
 const User = require('./models/user')
+const mongoSanitize = require('express-mongo-sanitize')
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp')
   .then(() => {
@@ -34,6 +35,7 @@ app.set('views', path.join(__dirname, 'views'))
 app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
 app.use(express.static(path.join(__dirname, 'public')))
+app.use(mongoSanitize())
 
 const sessionConfig = {
   secret: 'mysecret',
@@ -55,6 +57,7 @@ passport.deserializeUser(User.deserializeUser())
 app.use(flash())
 
 app.use((req, res, next) => {
+  console.log(req.query)
   res.locals.currentUser = req.user
   res.locals.success = req.flash('success')
   res.locals.error = req.flash('error')
